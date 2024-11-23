be33849dc71606417ca1c6eb0de093fb37e7e3ab
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-11-23 16:26:26 -0600

    Test and complete #play_game

--- lib/game_engine.rb
+++ lib/game_engine.rb
@@ -4,7 +4,22 @@ module OdinConnectFour
   # central logic for running connect four game
   class GameEngine
     def play_game
+      intro_game
+      rounds = 0
+      game_over = false
+      until game_over || rounds == 42
+        game_over = play_round
+        rounds += 1
+      end
       puts "player1 wins"
+    end
+
+    private
+
+    def intro_game
+      puts "The goal of connect four is to get 4 of your color in a horizontal,"
+      puts "vertical, or diagonal row. Enter the number of the column you want"
+      puts "to place your piece in."
     end
   end
 end
--- spec/game_engine_spec.rb
+++ spec/game_engine_spec.rb
@@ -3,16 +3,45 @@
 require_relative("../lib/game_engine")
 
 describe OdinConnectFour::GameEngine do
-  context "when game played" do
-    subject(:game_output) { described_class.new }
+  subject(:game) { described_class.new }
 
+  context "#play_game" do
     before do
-      allow(game_output).to receive(:puts)
+      allow(game).to receive(:puts)
+      allow(game).to receive(:intro_game)
+      allow(game).to receive(:play_round)
+    end
+
+    it "explains game" do
+      expect(game).to receive(:intro_game)
+      game.play_game
     end
 
     it "outputs winner" do
-      expect(game_output).to receive(:puts)
-      game_output.play_game
+      expect(game).to receive(:puts)
+      game.play_game
+    end
+
+    context "when third round ends game" do
+      before do
+        allow(game).to receive(:play_round).and_return(false, false, true)
+      end
+
+      it "stops when game is over" do
+        expect(game).to receive(:play_round).exactly(3).times
+        game.play_game
+      end
+    end
+
+    context "when no round ends game" do
+      before do
+        allow(game).to receive(:play_round).and_return(false)
+      end
+
+      it "stops after the 42nd round" do
+        expect(game).to receive(:play_round).exactly(42).times
+        game.play_game
+      end
     end
   end
 end
